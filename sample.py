6a8c2915485d8ed365bc8ee55f676731e4238575
msrks <mshrrks@gmail.com>
2018-05-25 19:55:52 +0900

    fix bug for raspberry pi

--- sample.py
+++ sample.py
@@ -1,9 +1,18 @@
 import time
 from glob import glob
+import platform
 
 from pydobot import Dobot
 
-available_ports = glob('/dev/cu*usb*')  # mask for OSX Dobot port
+def load():
+    if platform.system() == "Windows":
+        pass
+    elif platform.system() == "Darwin" :
+        available_ports = glob('/dev/cu*usb*')  # mask for OSX Dobot port
+    else:
+        available_ports = glob('/dev/ttyUSB*')  # mask for Raspi Dobot port
+
+
 if len(available_ports) == 0:
     print('no port found for Dobot Magician')
     exit(1)
@@ -11,9 +20,17 @@ if len(available_ports) == 0:
 device = Dobot(port=available_ports[0])
 
 time.sleep(0.5)
-device.speed(100)
+device.speed(50)
 device.go(250.0, 0.0, 25.0)
-device.speed(10)
+device.speed(50)
+device.go(250.0, 0.0, 0.0)
+device.speed(50)
+device.go(250.0, 0.0, 25.0)
+device.speed(50)
+device.go(250.0, 0.0, 0.0)
+device.speed(50)
+device.go(250.0, 0.0, 25.0)
+device.speed(50)
 device.go(250.0, 0.0, 0.0)
 time.sleep(2)
 device.close()
